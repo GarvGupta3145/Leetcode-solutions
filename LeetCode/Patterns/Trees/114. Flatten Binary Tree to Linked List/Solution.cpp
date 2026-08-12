ea1986b5241f51f86ab8901032bac397c68d0d43
Garv Gupta <garv3145gupta@gmail.com>
2026-08-12 23:21:59 +0530

    Add C++ Solution to Patterns/Trees

--- LeetCode/Patterns/Trees/114. Flatten Binary Tree to Linked List/Solution.cpp
+++ LeetCode/Patterns/Trees/114. Flatten Binary Tree to Linked List/Solution.cpp
@@ -1,31 +1,20 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ * APPROACH 3: Reverse PostOrder
+               right left root
  */
 class Solution {
 public:
-    void dfs(TreeNode* root,vector<TreeNode*>&preorder){
-        if(!root){
-            return;
-        }
-        preorder.push_back(root);
-        dfs(root->left,preorder);
-        dfs(root->right,preorder);
+    void solve(TreeNode*root,TreeNode* &prev){
+        if(!root)return;
+        solve(root->right,prev);
+        solve(root->left,prev);
+        root->right=prev;
+        root->left=NULL;
+        prev=root;
+        return;
     }
     void flatten(TreeNode* root) {
-        if(!root)return;
-        vector<TreeNode*>preorder;
-        dfs(root,preorder);
-        for(int i=1;i<preorder.size();i++){
-            preorder[i-1]->left=NULL;
-            preorder[i-1]->right=preorder[i];
-        }
+        TreeNode*prev=NULL;
+        solve(root,prev);
     }
 };
