419180df908bdcd16e71ec818b1ea90fd39f9a7e
Garv Gupta <garv3145gupta@gmail.com>
2026-08-12 09:57:34 +0530

    Add C++ Solution to Patterns/Sliding Window

---
+++ LeetCode/Patterns/Sliding Window/3225. Length of Longest Subarray With at Most K Frequency/Solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxSubarrayLength(vector<int>& nums, int k) {
+        unordered_map<int,int>mp;
+        int count=0;
+        int mx=0;
+        int l=0;
+        for(int i=0;i<nums.size();i++){
+            if(mp[nums[i]]<k){
+                mp[nums[i]]++;
+                count++;
+            }
+            else if(mp[nums[i]]==k){
+                while(l<=i && nums[l]!=nums[i]){
+                    mp[l]--;
+                    count--;
+                    l++;
+                }
+                l++;
+            }
+            mx=max(mx,count);
+        }
+        return mx;
+    }
+};
