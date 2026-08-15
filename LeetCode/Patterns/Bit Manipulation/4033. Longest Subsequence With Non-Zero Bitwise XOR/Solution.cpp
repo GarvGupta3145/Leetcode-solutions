ac1b4c5dc5725442d8230674cce5dff0b5487e91
Garv Gupta <garv3145gupta@gmail.com>
2026-08-15 22:18:50 +0530

    Add C++ Solution to Patterns/Bit Manipulation

--- LeetCode/Patterns/Bit Manipulation/4033. Longest Subsequence With Non-Zero Bitwise XOR/Solution.cpp
+++ LeetCode/Patterns/Bit Manipulation/4033. Longest Subsequence With Non-Zero Bitwise XOR/Solution.cpp
@@ -3,13 +3,13 @@ public:
     int longestSubsequence(vector<int>& nums) {
         int l=0;
         int n=nums.size();
-        bool a=true;;
+        int a=0;;
         for(int i=0;i<nums.size();i++){
             l=l^nums[i];
-            if(nums[i]!=0)a=true;
+            if(nums[i]==0)a++;
         }
-        if(l!=0)return n;
-        if(l==0 && a==true)return n-1;
-        return 0;
+        if(a==n)return 0;
+        if(l==0)return n-1;
+        return n;
     }
-};
+}; 
