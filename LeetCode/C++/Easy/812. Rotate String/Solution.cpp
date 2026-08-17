76888d70e53374439c7e48f9a597de04d7f73620
Garv Gupta <garv3145gupta@gmail.com>
2026-08-17 12:35:45 +0530

    Add Easy C++ Solution from LeetCode

---
+++ LeetCode/C++/Easy/812. Rotate String/Solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+         if (s.length() != goal.length()) return false;
+        
+        // Concatenate s with itself and check if goal exists in it
+        string doubledS = s + s;
+        return doubledS.find(goal) != string::npos;
+    }
+};
