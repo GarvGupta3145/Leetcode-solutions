41a44fa822c96cc96d95ac5fc5521a544e45abdd
Garv Gupta <garv3145gupta@gmail.com>
2026-08-19 10:16:46 +0530

    Add Medium C++ Solution from LeetCode

---
+++ LeetCode/C++/Medium/1114. Binary Search Tree to Greater Sum Tree/Solution.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void build(TreeNode*root,TreeNode*&prev){
+        if(!root)return;
+        build(root->right,prev);
+        if(prev){
+            root->val=root->val+prev->val;
+        }
+        prev=root;
+        build(root->left,prev);
+    }
+    TreeNode* bstToGst(TreeNode* root) {
+        TreeNode* prev=NULL;
+        build(root,prev);
+        return root;
+    }
+};
