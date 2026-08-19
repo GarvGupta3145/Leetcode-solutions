4e3a0320050919db92dd4e2ccc8dc51b3200fda3
Garv Gupta <garv3145gupta@gmail.com>
2026-08-19 12:25:31 +0530

    Add C++ Solution to Patterns/Trees

--- LeetCode/Patterns/Trees/103. Binary Tree Zigzag Level Order Traversal/Solution.cpp
+++ LeetCode/Patterns/Trees/103. Binary Tree Zigzag Level Order Traversal/Solution.cpp
@@ -6,44 +6,50 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>>ans;
-        if(!root)return ans;
-        ans.push_back({root->val});
-        queue<TreeNode*>q;
-        q.push(root);
-        vector<int>res;
-        bool flag=false;
-        while(!q.empty()){
-            vector<int>temp;
-            int n=q.size();
-            for(int i=0;i<n;i++){
-                TreeNode* node=q.front();
-                if(node->left){
-                    temp.push_back(node->left->val);
-                    q.push(node->left);
+        vector<vector<int>> result;
+        if (!root)
+            return result;
+
+        deque<TreeNode*> q;
+        q.push_back(root);
+        bool leftToRight = true;
+
+        while (!q.empty()) {
+            int size = q.size();
+            vector<int> level(size);
+
+            for (int i = 0; i < size; i++) {
+                TreeNode* node;
+                if (leftToRight) {
+                    node = q.front();
+                    q.pop_front();
+                    level[i] = node->val;
+                    if (node->left)
+                        q.push_back(node->left);
+                    if (node->right)
+                        q.push_back(node->right);
+                } else {
+                    node = q.back();
+                    q.pop_back();
+                    level[i] = node->val;
+                    if (node->right)
+                        q.push_front(node->right);
+                    if (node->left)
+                        q.push_front(node->left);
                 }
-                if(node->right){
-                    temp.push_back(node->right->val);
-                    q.push(node->right);
-                }
-                q.pop();
             }
-            if(!temp.empty()){
-                if(flag)ans.push_back(temp);
-                else {
-                    reverse(temp.begin(), temp.end());
-                    ans.push_back(temp);
-                }
-            }
-            flag=!flag;
+
+            result.push_back(level);
+            leftToRight = !leftToRight;
         }
-        return ans;
+
+        return result;
     }
 };
