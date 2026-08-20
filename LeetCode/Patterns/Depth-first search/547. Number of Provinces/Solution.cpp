4b6611a60fb356f3bc9eca4e42be60708009d2a3
Garv Gupta <garv3145gupta@gmail.com>
2026-08-20 11:48:52 +0530

    Add C++ Solution to Patterns/Depth-first search

--- LeetCode/Patterns/Depth-first search/547. Number of Provinces/Solution.cpp
+++ LeetCode/Patterns/Depth-first search/547. Number of Provinces/Solution.cpp
@@ -11,7 +11,7 @@ private:
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n=isConnected.size();
-        vector<int>visited(n+1,0);
+        vector<int>visited(n,0);
         int provinces=0;
         for(int i=0;i<n;i++){
             if(!visited[i]){
