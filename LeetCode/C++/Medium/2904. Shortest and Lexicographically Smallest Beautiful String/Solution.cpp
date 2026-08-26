c0c587e290d96c6a70ef27ac2f1aba7d56c4a6ce
Garv Gupta <garv3145gupta@gmail.com>
2026-08-26 22:18:45 +0530

    Add Medium C++ Solution for 2904. Shortest and Lexicographically Smallest Beautiful String from LeetCode

---
+++ LeetCode/C++/Medium/2904. Shortest and Lexicographically Smallest Beautiful String/Solution.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string shortestBeautifulSubstring(string s, int k) {
+        int l=0,r=0;
+        int nm=INT_MAX;
+        int count=0;
+        string st;
+
+        while(l<=r && r <s.size()){
+            while(l<s.size() && s[l]!='1'){
+                if(l==r){
+                    r++;
+                }
+                l++;
+            }
+            if(s[r]=='1'){
+                count++;
+                if(count==k){
+                    if(nm>=r-l+1){
+                        st=s.substr(l,r-l+1);
+                        nm=r-l+1;
+                    }
+                    count-=1;
+                    l++;
+                    while(l<s.size() && s[l]!='1'){
+                        if(l==r){
+                            r++;
+                        }
+                        l++;
+                    }
+                }
+            }
+            r++;
+        }
+        return st;
+    }
+};
