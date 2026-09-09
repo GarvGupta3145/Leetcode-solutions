3a0a4b136b3bd41e027d71875b404b481fc5d86d
Garv Gupta <garv3145gupta@gmail.com>
2026-09-09 10:21:48 +0530

    Add Medium C++ Solution for 3871. Count Commas in Range II from LeetCode

--- LeetCode/C++/Medium/3871. Count Commas in Range II/Solution.cpp
+++ LeetCode/C++/Medium/3871. Count Commas in Range II/Solution.cpp
@@ -5,7 +5,7 @@ public:
     
     // 528,580,435,893
     // count=12
-
+    // 999,999=999,999-1,000
     long long countCommas(long long n) {
         int count=0;
         long long d=n;
@@ -17,9 +17,7 @@ public:
         int i=3;
         int j=1;
         long long ans=0;
-        int a=3;
         while(i<count){
-            return a;
             if(count-i>3){
                 //1,112,112 c=7
                 ans+=(pow(10,i+3)-pow(10,i))*j;
@@ -28,10 +26,10 @@ public:
                 //j=2,i=6
              }
              else{
-                ans=ans+1+((n-pow(10,i))*j);
+                ans=ans+((n-pow(10,i))*j);
                 break;
              }
         }
-        return ans;
+        return ans+1;
     }
 };
